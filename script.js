5c04df686f080d6eab75afed5d1b16c35b4c519e
Mike Fields <mikefields81@gmail.com>
2018-05-20 14:45:47 -0500

    index 3 carousel

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
 
     console.log("script.js is running");
 
-    $('.regular').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+    $(".regular").slick({
         dots: true,
         infinite: true,
-        centerMode: true
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000
     });
+
+    // $('.regular').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //     dots: true,
+    //     infinite: true,
+    //     centerMode: true
+    // });
 });
